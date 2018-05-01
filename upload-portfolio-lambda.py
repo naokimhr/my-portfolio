8a9d4762591194c63b5f9de0bb41a94957cf8d6a
Naoki Mihara <naokimhr@gmail.com>
2018-05-01 17:21:54 +0800

    added sns notification publish and simple error handling

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,21 +6,30 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:329861036217:deployPortfolioTopic')
+
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        
+        portfolio_bucket = s3.Bucket('portfolio.naokimihara.com')
+        build_bucket = s3.Bucket('portfoliobuild.naokimihara.com')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+          
+        print "Job done!"
+        
+        topic.publish(Subject="Portfolio Deployed",Message="Portfolio Deployed Successfully.")
+    except:
+        topic.publish(Subject="Portfolio Deployed Error",Message="Portfolio was not deployed Successfully.")
+        raise
     
-    portfolio_bucket = s3.Bucket('portfolio.naokimihara.com')
-    build_bucket = s3.Bucket('portfoliobuild.naokimihara.com')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-      
-    print "Job done!"
-            
     return 'Hello from lambda'

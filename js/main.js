e17d178f193d08d10029d5a3e6e2f3021a8d5d8a
Naoki Mihara <naokimhr@gmail.com>
2018-05-15 02:04:09 +0800

    Move static markup for work example

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work.js';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Work Example1",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/koifish001.jpg",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example2",
+    'image': {
+      'desc': "example screenshot of a project involving chemistry",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example3",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example2.png",
+      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+           https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  }
+]
 
-console.log("Webpack works");
-
-
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
